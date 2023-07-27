861b7711e62ecb24d76ae25c23da0dc268e2ecf5
filin0wl <filin.owls@gmail.com>
2023-07-27 19:33:42 +0300

    add solution

--- MarathonUIKitNinthTask/ViewController.swift
+++ MarathonUIKitNinthTask/ViewController.swift
@@ -1,19 +1,97 @@
-//
-//  ViewController.swift
-//  MarathonUIKitNinthTask
-//
-//  Created by Anastasiia Perminova on 27.07.2023.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    // MARK: Properties & Constants
+
+    let cellWidth: CGFloat = 250
+    let cellHeight: CGFloat = 370
+    let horizontalInsets: CGFloat = 10
+    let verticalInsets: CGFloat = 15
+
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+
+    // MARK: Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .white
+        title = "Collection"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        setupCollectionView()
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
+        setupConstraints()
+    }
+
+    // MARK: Setup
+
+    func setupCollectionView() {
+
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = verticalInsets
+        collectionView.collectionViewLayout = layout
+
+        collectionView.showsHorizontalScrollIndicator = true
+        collectionView.isScrollEnabled = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "SimpleCell")
+        view.addSubview(collectionView)
+    }
+
+    func setupConstraints() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0)
+        ])
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: cellWidth, height: cellHeight)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: horizontalInsets, left: verticalInsets, bottom: horizontalInsets, right: verticalInsets)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SimpleCell", for: indexPath)
+
+        cell.layer.cornerRadius = 10
+        cell.layer.cornerCurve = .continuous
+        cell.backgroundColor = .purple
+
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegateFlowLayout
+
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+            let old = targetContentOffset.pointee.x
+            let offset = round(old / (cellWidth + verticalInsets))
+            let new = offset * (cellWidth + verticalInsets)
+            targetContentOffset.pointee.x = new
+        }
 }
-
